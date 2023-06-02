865ef79ef37124c1e7c12bccfade835fd57ff1c7
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-06-02 16:29:25 -0300

    peticiones get y delete

--- index.js
+++ index.js
@@ -33,6 +33,24 @@ app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+  
+  if (note) {
+    res.json(note)
+  }else{
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running en port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running en port ${PORT}`)
+})
