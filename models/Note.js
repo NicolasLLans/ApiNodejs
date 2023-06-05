6d781740d36ece5a114a05ed50900c61ab6a2bd5
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-06-04 23:30:22 -0300

    cree el modelo de usuario

--- models/Note.js
+++ models/Note.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose')
 const noteSchema = new mongoose.Schema({
-      content: String,
-      date: Date,
-      importante: Boolean
+  content: String,
+  date: Date,
+  importante: Boolean,
+  user: [{
+    type: mongoose.Types.ObjectId,
+    ref: 'User'
+  }]
 })
 
 noteSchema.set('toJSON', {
-      transform:(document, returnedObject) => {
-            returnedObject.id = returnedObject._id
-            delete returnedObject._id
-            delete returnedObject.__v
-      }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-const Note= mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema)
 
 module.exports = Note
